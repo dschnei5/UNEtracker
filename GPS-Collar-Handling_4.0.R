7810cc7be3aecdb2af0623f13b50869323b69039
Rocky Schneider <dschnei5@une.edu.au>
2020-06-29 22:02:52 +1000

    Error handling completed

--- GPS-Collar-Handling_4.0.R
+++ GPS-Collar-Handling_4.0.R
@@ -59,6 +59,61 @@ tm.correction <- function(x,y) {
     } else {out <- as_datetime(x, tz = "AEST")}
   return(out)
 };
+gps.processor <- function(x) {
+  tmp1.df <- x
+  colnames(tmp1.df) <- c("GPGGA","UTC_Time","Lat","N/S1","Long","E/W1","Fix","Satellites","HDOP","Altitude","M","g.g","M","Unknown","Tag","$GPRMC","Time2","A","Latitude","N/S2","Longitude","E/W2","Speed","Course","UTC_Date","Blank","Checksum","Unk1","Unk2","Unk3");
+  assign(dname[[i]],tmp1.df[,-c(1,4,6,11,12,13,14,15,16,17,18,19,20,21,22,23,24,26,27,28,29,30)]);
+  tmp2.df <- get(dname[[i]]);
+  tmp2.df$UTC_Time <- suppressWarnings(as.numeric.factor(tmp2.df$UTC_Time));
+  tmp2.df$UTC_Time <- ifelse(tmp2.df$UTC_Time <= 99.99, paste0("0000",tmp2.df$UTC_Time),
+                             ifelse(tmp2.df$UTC_Time <= 999.99, paste0("000",tmp2.df$UTC_Time),
+                                    ifelse(tmp2.df$UTC_Time <=9999.99, paste0("00",tmp2.df$UTC_Time),
+                                           ifelse(tmp2.df$UTC_Time <= 99999.99, paste0("0", tmp2.df$UTC_Time),
+                                                  tmp2.df$UTC_Time))));
+  if(is.factor(tmp2.df$UTC_Date)){
+  tmp2.df$UTC_Date <- suppressWarnings(as.numeric.factor(tmp2.df$UTC_Date));
+  };
+  tmp2.df$UTC_Date <- ifelse(tmp2.df$UTC_Date < 99999, paste0("0",tmp2.df$UTC_Date), tmp2.df$UTC_Date);
+  tmp2.df <- cbind(tmp2.df, as.character(paste(tmp2.df$UTC_Date,tmp2.df$UTC_Time)));
+  colnames(tmp2.df) <- c("UTC_Time","Lat","Long","Fix","Satellites","HDOP","Altitude","UTC_Date", "UTC_DateTime");
+  tmp2.df <- suppressWarnings(cbind(tmp2.df, dmy_hms(tmp2.df$UTC_DateTime, tz = "GMT")));
+  tmp2.df <- tmp2.df[,-(9)];
+  colnames(tmp2.df) <- c("UTC_Time","Lat","Long","Fix","Satellites","HDOP","Altitude","UTC_Date", "UTC_DateTime");
+  tmp2.df$EST_DateTime <- with_tz(tmp2.df$UTC_DateTime,"Australia/Brisbane");
+  tmp2.df <- tmp2.df[!is.na(tmp2.df$UTC_DateTime),];
+  tmp2.df$EST_DateTime <- apply(X = as.array(tmp2.df$EST_DateTime), MARGIN = 1, FUN = tm.correction, y = tm.diff);
+  tmp2.df$EST_DateTime <- as_datetime(tmp2.df$EST_DateTime, tz = "Australia/Brisbane")
+  tmp2.df$EST_Hour <- hour(tmp2.df$EST_DateTime);
+  suppressWarnings(tmp2.df$SecInt <- tmp2.df$UTC_DateTime[-1] - tmp2.df$UTC_DateTime);
+  tmp2.df$SecInt <- as.numeric(tmp2.df$SecInt);
+  tmp2.df$Long <- as.character(tmp2.df$Long);
+  tmp2.df$Long <- as.numeric(tmp2.df$Long);
+  tmp2.df$Lat <- as.character(tmp2.df$Lat);
+  tmp2.df$Lat <- as.numeric(tmp2.df$Lat);
+  tmp2.df$Long.con <- tmp2.df$Long/100;
+  tmp2.df$Lat.con <- tmp2.df$Lat/100;
+  tmp2.df$Long.con1 <- as.integer(tmp2.df$Long.con);
+  tmp2.df$Lat.con1 <- as.integer(tmp2.df$Lat.con);
+  tmp2.df$Long.con2 <- (tmp2.df$Long.con-tmp2.df$Long.con1)*100;
+  tmp2.df$Lat.con2 <- (tmp2.df$Lat.con-tmp2.df$Lat.con1)*100;
+  tmp2.df$Longitude <- (tmp2.df$Long.con1+(tmp2.df$Long.con2/60));
+  tmp2.df$Latitude <- -(tmp2.df$Lat.con1+(tmp2.df$Lat.con2/60));
+  tmp2.df <- tmp2.df[,-c(2,3,13,14,15,16,17,18)];
+  tmp2.df$Longitude <- ifelse(tmp2.df$Longitude <= 1,NA,tmp2.df$Longitude);
+  tmp2.df <- tmp2.df[!is.na(tmp2.df$Longitude),]
+  tmp2.df$Latitude <- ifelse(tmp2.df$Latitude >= 1,NA,tmp2.df$Latitude);
+  tmp2.df <- tmp2.df[!is.na(tmp2.df$Latitude),];
+  tmp2.df$Longitude <- as.numeric(tmp2.df$Longitude);
+  tmp2.df$Latitude <- as.numeric(tmp2.df$Latitude);
+  coords <- SpatialPoints(data.frame(X = tmp2.df$Longitude,Y = tmp2.df$Latitude),CRS("+proj=longlat +datum=WGS84"))
+  d.UTM <- spTransform(coords, UCRS.new);
+  tmp2.df <- cbind(tmp2.df,d.UTM@coords);
+  suppressWarnings(tmp2.df$StepLength <- sqrt((tmp2.df$X[-1] - tmp2.df$X)^2+(tmp2.df$Y[-1] - tmp2.df$Y)^2));
+  tmp2.df$Speed <- tmp2.df$StepLength/as.numeric(tmp2.df$SecInt);
+  tmp2.df$File <- files[i];
+  tmp2.df$CollarID <- i;
+  assign(dname[[i]],tmp2.df, envir = .GlobalEnv);
+}
 
 ####Y2K + 20 Bug Fix####
 
@@ -102,25 +157,42 @@ print(paste("Coordinate Reference System set to:",UCRS.new@projargs));
 
 Sys.sleep(2);
 
-####Read in Data Files and Process####
+####Read in Data Files Check for Errors####
 
 error.log <- data.frame(File = NA,
                         Problem = NA,
                         stringsAsFactors = FALSE);
 
 for (i in seq(along=files)) {
-  #i=2
-  {dname[i] <- paste("data",i, sep = ".");
+  #i=3
+  dname[i] <- paste("data",i, sep = ".");
   suppressWarnings(assign(dname[[i]], read.csv(files[i], header = FALSE)));
   tmp1.df <- get(dname[[i]]);
   GGA.tmp <- subset(tmp1.df, V1=="$GPGGA");
   RMC.tmp <- subset(tmp1.df, V1=="$GPRMC");
   
-  if (dim(GGA.tmp)[1] != dim(RMC.tmp)[1] | dim(RMC.tmp)[1] == 0) {
-    message(paste("File", files[i], "has varying lengths of GGA and RMC records or no data, skipping file..."));
+  if (dim(RMC.tmp)[1] == 0) {
+    message(paste("File", files[i], "has no data, skipping file..."));
     rm(list = dname[[i]]);
-    error.log[i,] <- c(files[i],"varying lengths of GGA and RMC records or no data");}
-  else {
+    error.log[i,] <- c(files[i],"ha no data");
+  } else if (dim(GGA.tmp)[1] != dim(RMC.tmp)[1]) {
+    message(paste("File", files[i], "has varying lengths of GGA and RMC records or no data, attempting to fix..."));
+    Sys.sleep(1);
+    tmp1.df <- merge(GGA.tmp,RMC.tmp,"V2")
+    if (length(tmp1.df) != 29) {
+      rm(list = dname[[i]]);
+      error.log[i,] <- c(files[i],"varying lengths of GGA and RMC records, fix failed");
+      } else {
+      tmp1.df <- tmp1.df[,c(2,1,3:16,1,17:29)]
+      tmp1.df[,3] <- suppressWarnings(as.numeric.factor(tmp1.df[,3]))
+      tmp1.df[,19] <- suppressWarnings(as.numeric.factor(tmp1.df[,19]))
+      tmp1.df <- tmp1.df[tmp1.df[,3]==tmp1.df[,19],]
+      tmp1.df <- tmp1.df[complete.cases(tmp1.df[,19]),]
+      error.log[i,] <- c(files[i],"varying lengths of GGA and RMC records fixed, some data lost");
+      message(paste0("File ",files[i],"'s varying lengths of GGA and RMC records fixed, some data lost"))
+      gps.processor(tmp1.df)
+      }
+    } else {
     print(paste("File",files[i], "Successfully Imported"));
     error.log[i,] <- c(files[i],"Successfully Imported")
     tmp1.df <- cbind(GGA.tmp, RMC.tmp);
@@ -129,61 +201,13 @@ for (i in seq(along=files)) {
       rm(list = dname[[i]]);
       error.log[i,] <- c(files[i],"import error, columns not delimited correctly, skipping file");
     } else {
-      colnames(tmp1.df) <- c("GPGGA","UTC_Time","Lat","N/S1","Long","E/W1","Fix","Satellites","HDOP","Altitude","M","g.g","M","Unknown","Tag","$GPRMC","Time2","A","Latitude","N/S2","Longitude","E/W2","Speed","Course","UTC_Date","Blank","Checksum","Unk1","Unk2","Unk3");
-      assign(dname[[i]],tmp1.df[,-c(1,4,6,11,12,13,14,15,16,17,18,19,20,21,22,23,24,26,27,28,29,30)]);
-      tmp2.df <- get(dname[[i]]);
-      tmp2.df$UTC_Time <- suppressWarnings(as.numeric.factor(tmp2.df$UTC_Time));
-      tmp2.df$UTC_Time <- ifelse(tmp2.df$UTC_Time <= 99.99, paste0("0000",tmp2.df$UTC_Time),
-                               ifelse(tmp2.df$UTC_Time <= 999.99, paste0("000",tmp2.df$UTC_Time),
-                                      ifelse(tmp2.df$UTC_Time <=9999.99, paste0("00",tmp2.df$UTC_Time),
-                                             ifelse(tmp2.df$UTC_Time <= 99999.99, paste0("0", tmp2.df$UTC_Time),
-                                                    tmp2.df$UTC_Time))));
-      #tmp2.df$UTC_Time <- as.numeric(tmp2.df$UTC_Time)
-      tmp2.df$UTC_Date <- ifelse(tmp2.df$UTC_Date < 99999, paste0("0",tmp2.df$UTC_Date), tmp2.df$UTC_Date);
-      tmp2.df <- cbind(tmp2.df, as.character(paste(tmp2.df$UTC_Date,tmp2.df$UTC_Time)));
-      colnames(tmp2.df) <- c("UTC_Time","Lat","Long","Fix","Satellites","HDOP","Altitude","UTC_Date", "UTC_DateTime");
-      tmp2.df <- cbind(tmp2.df, dmy_hms(tmp2.df$UTC_DateTime, tz = "GMT"));
-      tmp2.df <- tmp2.df[,-(9)];
-      colnames(tmp2.df) <- c("UTC_Time","Lat","Long","Fix","Satellites","HDOP","Altitude","UTC_Date", "UTC_DateTime");
-      tmp2.df$EST_DateTime <- with_tz(tmp2.df$UTC_DateTime,"Australia/Brisbane");
-      tmp2.df$EST_DateTime <- apply(X = as.array(tmp2.df$EST_DateTime), MARGIN = 1, FUN = tm.correction, y = tm.diff);
-      tmp2.df$EST_DateTime <- as_datetime(tmp2.df$EST_DateTime, tz = "AEST")
-      tmp2.df$EST_Hour <- hour(tmp2.df$EST_DateTime);
-      suppressWarnings(tmp2.df$SecInt <- tmp2.df$UTC_DateTime[-1] - tmp2.df$UTC_DateTime);
-      tmp2.df$SecInt <- as.numeric(tmp2.df$SecInt);
-      tmp2.df$Long <- as.character(tmp2.df$Long);
-      tmp2.df$Long <- as.numeric(tmp2.df$Long);
-      tmp2.df$Lat <- as.character(tmp2.df$Lat);
-      tmp2.df$Lat <- as.numeric(tmp2.df$Lat);
-      tmp2.df$Long.con <- tmp2.df$Long/100;
-      tmp2.df$Lat.con <- tmp2.df$Lat/100;
-      tmp2.df$Long.con1 <- as.integer(tmp2.df$Long.con);
-      tmp2.df$Lat.con1 <- as.integer(tmp2.df$Lat.con);
-      tmp2.df$Long.con2 <- (tmp2.df$Long.con-tmp2.df$Long.con1)*100;
-      tmp2.df$Lat.con2 <- (tmp2.df$Lat.con-tmp2.df$Lat.con1)*100;
-      tmp2.df$Longitude <- (tmp2.df$Long.con1+(tmp2.df$Long.con2/60));
-      tmp2.df$Latitude <- -(tmp2.df$Lat.con1+(tmp2.df$Lat.con2/60));
-      tmp2.df <- tmp2.df[,-c(2,3,13,14,15,16,17,18)];
-      tmp2.df$Longitude <- ifelse(tmp2.df$Longitude <= 1,NA,tmp2.df$Longitude);
-      tmp2.df <- tmp2.df[!is.na(tmp2.df$Longitude),]
-      tmp2.df$Latitude <- ifelse(tmp2.df$Latitude >= 1,NA,tmp2.df$Latitude);
-      tmp2.df <- tmp2.df[!is.na(tmp2.df$Latitude),];
-      tmp2.df$Longitude <- as.numeric(tmp2.df$Longitude);
-      tmp2.df$Latitude <- as.numeric(tmp2.df$Latitude);
-      coords <- SpatialPoints(data.frame(X = tmp2.df$Longitude,Y = tmp2.df$Latitude),CRS("+proj=longlat +datum=WGS84"))
-      d.UTM <- spTransform(coords, UCRS.new);
-      tmp2.df <- cbind(tmp2.df,d.UTM@coords);
-      suppressWarnings(tmp2.df$StepLength <- sqrt((tmp2.df$X[-1] - tmp2.df$X)^2+(tmp2.df$Y[-1] - tmp2.df$Y)^2));
-      tmp2.df$Speed <- tmp2.df$StepLength/as.numeric(tmp2.df$SecInt);
-      tmp2.df$File <- files[i];
-      tmp2.df$CollarID <- i;
-      assign(dname[[i]],tmp2.df);
+      gps.processor(tmp1.df)
     }
-  }
   }
   }# End of i loop
 
-rm(tmp1.df,tmp2.df,GGA.tmp,RMC.tmp,coords,d.UTM,dname,i);
+
+rm(tmp1.df,GGA.tmp,RMC.tmp,dname,i);
 
 ####Create Output DF####
 
